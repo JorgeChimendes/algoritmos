9f8fe06ac7e2ace1e0bdcc1f9f0b00befee51d25
JorgeChimendes <jorgebf3cod@hotmail.com>
2016-03-15 19:16:15 -0300

    moddy by jorge

--- ExercícioT12.c
+++ ExercícioT12.c
@@ -17,8 +17,10 @@ int main (){
 	else{
 		c = n2;
 	}
+
 	printf("O valor de N1 é: %d\n", n1);
 	printf("O valor de N2 é: %d\n", n2);
+
 	if(n1>n2){
 		printf("O maior valor é: %i.\n E o menor valor é: %i\n",n1,n2);
 	}
@@ -28,8 +30,9 @@ int main (){
 	else{
 		printf("O maior valor é: %i.\n E o menor valor é: %i\n",n2,n1);
 	}
-		printf("================================\n");
+		
 	//LEITURA DOS NOMES
+		printf("================================\n");
 		printf("Digite o seu nome(sem acentos):\n");
 		fgets(user,NOMES,stdin);
 		printf("Digite o nome do seu pai(sem acentos):\n");
@@ -37,12 +40,35 @@ int main (){
 		printf("Digite o nome da sua mae(sem acentos):\n");
 		fgets(mae,NOMES,stdin);
 		printf("================================\n");
-	if(n1>n2){
+	
+		if(n1>n2){
 		dif = n1-n2;
-	}else{
-		dif = n2-n1;
+		if ( (n2 % 2) == 0) {
+			printf("N2 e um numero par \n");
+		}else{
+			printf("N2 e um numero impar \n");
+		}			
 	}
+	else{
+		dif = n2-n1;
+		if ((n1 % 2) == 0) {
+			printf("N1 e um numero par \n");
+		}
+		else {
+			printf("N1 e um numero impar \n");
+		}
+	}
+		
 	for(i=0;i < VET;i++){
+		do {
+			printf("digite um valor: ");
+			scanf("%d", &v[i]);
+			printf("\n");			
+			if (v[i] < 0) {
+				printf("error! valores negativos nao podem ser inseridos.\n\n");
+			}
+		} while (v[i] < 0);
+		//feito no t11 <t11>
 		if(i == 0){
 			v[0]=0;
 		}
@@ -53,6 +79,7 @@ int main (){
 		if(v[i] > 1000){
 			v[i]=i;
 		}
+		//</t11>
 	}
 	printf("\n==========================\n");
 	// CALCULAR SE OS NUMEROS DO VETOR SAO PARES OU IMPARES E PRIMOS
